707ca7491ec16421a6e5444f451c401c13519e25
Asim Dhakal <asimdkal@gmail.com>
2025-08-02 21:21:23 +0545

    Update manage.py
    adding comment
    
    Co-Authored-By: YamaSD <124194767+YamaSD@users.noreply.github.com>

--- backend/manage.py
+++ backend/manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+# This is the main function for the manage part of backend 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thinkthread.settings')
